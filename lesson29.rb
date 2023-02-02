101502a073501861afa769e47fc4eb1ce98bf0c0
dave2281 <dava.bigboy@gmail.com>
2023-02-02 09:52:12 +0200

    lesson29.rb was done

---
+++ lesson29.rb
@@ -0,0 +1,25 @@
+arr = [1, 4, 1212, 2, 17, 89, 201, 453]
+
+max_value = 0
+
+# for arr_value in arr do
+#   if arr_value > max_value
+#     max_value = arr_value
+#   end
+# end
+
+# arr.each do |arr_value|
+#   if arr_value > max_value
+#     max_value = arr_value
+#   end
+# end
+
+# arr.map do |arr_value|
+#   if arr_value > max_value
+#     max_value = arr_value
+#   end
+# end
+
+# .collect
+
+p max_value.inspect
