17a361b16849148c0456672068e6e492e35ab840
dave2281 <dava.bigboy@gmail.com>
2023-02-07 10:16:47 +0200

    for in method was deleted

--- lesson29.rb
+++ lesson29.rb
@@ -2,12 +2,6 @@ arr = [0, 4, 1212, 2, 17, 89, 201, 453]
 
 max_value = 0
 
-# for arr_value in arr do
-#   if arr_value > max_value
-#     max_value = arr_value
-#   end
-# end
-
 # arr.each do |arr_value|
 #   if arr_value > max_value
 #     max_value = arr_value
