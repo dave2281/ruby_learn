f79d9fdd959643cb9ee6016f25fa59ba60f26d93
dave2281 <82700142+dave2281@users.noreply.github.com>
2023-02-13 20:17:23 +0200

    Delete lesson65 directory

--- lesson65/main.rb
+++
@@ -1,7 +0,0 @@
-current_path = File.dirname(__FILE__)
-time = Time.now
-time_hours_min = time.strftime("%Y-%m-%d")
-file = File.new(current_path + '/' + "dave" + '.txt', 'a:UTF-8')
-file.puts("Hello from ruby")
-file.close
-
