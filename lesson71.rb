6c9244dff1208994b90a53cbaab84c9f1abc9133
dave2281 <dava.bigboy@gmail.com>
2023-02-13 15:44:56 +0200

    added some changes and lesson72

--- lesson71.rb
+++ lesson71.rb
@@ -1,5 +1,5 @@
-email = 'davabigboy@gmail.com'
-email_regexp = /[a-z0-9\.]+@[a-z0-9]+\.[a-z]+/
+email = 'dava.bigboy@gmail.com'
+email_regexp = /[a-z0-9\.]+\@[a-z0-9]+\.[a-z]+/
 
 if email.match(email_regexp)
   puts 'Cool'
