6a21ce5e9128ab443f630c08626540fa54a803ca
dave2281 <dava.bigboy@gmail.com>
2023-02-14 08:40:50 +0200

    ???

--- wars/tasks/4_kyu/middle_permutations.rb
+++ wars/tasks/4_kyu/middle_permutations.rb
@@ -1,13 +1,7 @@
 def middle_permutation(string)
   a = string.chars
   arr_permutations = a.permutation.map {|arrs| arrs.join}
-  p arr_permutations.size
-  if arr_permutations.size < 720
-    return arr_permutations[arr_permutations.size/2.round-1]
-  elsif arr_permutations.size <= 720
-    return arr_permutations[arr_permutations.size/2.round-25]#
-  elsif arr_permutations.size >= 5030
-    return arr_permutations[arr_permutations.size/2.round-9]
-  end
+  
 end
-p middle_permutation('abcdxgz')
+
+p middle_permutation('pweafgnjdovyuxkhmr')
