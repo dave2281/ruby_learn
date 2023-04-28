d0e981937c965e54920534ed7c324b315ef7f033
dave2281 <dava.bigboy@gmail.com>
2023-04-28 21:08:35 +0300

    sql+ruby

--- ruby_lessons/lesson122/post.rb
+++ ruby_lessons/lesson122/post.rb
@@ -1,127 +1,54 @@
 require 'sqlite3'
 
+# Базовый класс «Запись» — здесь мы определим основные методы и свойства,
+# общие для всех типов записей.
 class Post
-
-  @@SQLITE_DB_FILE = '/home/dave/ruby_git/ruby_learn/ruby_lessons/lesson122/notepad.sqlite'
-
+  SQLITE_DB_FILE = 'notepad.sqlite'.freeze
 
   def self.post_types
     {'Memo' => Memo, 'Task' => Task, 'Link' => Link}
   end
 
-
   def self.create(type)
-    return post_types[type].new
+    post_types[type].new
   end
 
- def initialize
-    @created_at = Time.now 
-    @text = nil 
+  def initialize
+    @created_at = Time.now
+    @text = []
   end
 
-  def read_from_console
-  end
+  # Метод класса find_by_id находит в базе запись по идентификатору
+  def self.find_by_id(id)
+    # Если id не передали, мы ничего не ищем, а возвращаем nil
+    return if id.nil?
 
-  def to_strings
-  end
+    # Если id передали, едем дальше
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+    db.results_as_hash = true
 
-  def to_db_hash
-
-    {
-
-      'type' => self.class.name,
-      'created_at' => @created_at.to_s
-    }
-
-  end
-
-  def save_to_db
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE) 
-    db.results_as_hash = true 
-
-    db.execute(
-      "INSERT INTO posts (" +
-        to_db_hash.keys.join(', ') + 
-        ") " +
-        " VALUES ( " +
-        ('?,'*to_db_hash.keys.size).chomp(',') + 
-        ")",
-      to_db_hash.values 
-    )
-
-    insert_row_id = db.last_insert_row_id
+    # Начинаем аккуратно тянуть данные из базы методом execute
+    begin
+      result = db.execute('SELECT * FROM posts WHERE  rowid = ?', id)
+    rescue SQLite3::SQLException => e
+      # Если возникла ошибка, пишем об этом пользователю и выводим текст ошибки
+      puts "Не удалось выполнить запрос в базе #{SQLITE_DB_FILE}"
+      abort e.message
+    end
 
     db.close
 
-    return insert_row_id
+    return nil if result.empty?
+
+    result = result[0]
+
+    post = create(result['type'])
+    post.load_data(result)
+    post
   end
 
-  def load_data(data_hash)
-    @created_at = Time.parse(data_hash['created_at'])
-  end
-
-  def save
-    file = File.new(file_path, "w:UTF-8")
-
-    for item in to_strings do
-      file.puts(item)
-    end
-
-    file.close
-  end
-
-  def file_path
-    current_path = File.dirname(__FILE__)
-
-    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
-
-    return current_path + "/" + file_name
-  end
-
-  def self.find(limit, type, id)
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE) 
-    if !id.nil?
-      db.results_as_hash = true 
-      result = db.execute("SELECT * FROM posts WHERE  rowid = ?", id)
-
-      result = result[0] if result.is_a? Array
-      db.close
-
-      if result.empty?
-        puts "Такой id #{id} не найден в базе :("
-        return nil
-      else
-        post = create(result['type'])
-
-        post.load_data(result)
-
-        return post
-      end
-
-    else
-
-      db.results_as_hash = false
-
-      query = "SELECT rowid, * FROM posts "
-
-      query += "WHERE type = :type " unless type.nil?
-      query += "ORDER by rowid DESC " 
-
-      query += "LIMIT :limit " unless limit.nil? 
-
-      statement = db.prepare query
-
-      statement.bind_param('type', type) unless type.nil? 
-      statement.bind_param('limit', limit) unless limit.nil? 
-
-      result = statement.execute!
-      statement.close
-      db.close
-
-      return result
-    end
-  end
-
+  # Метод класса find_all возвращает массив записей из базы данных, который
+  # можно например показать в виде таблицы на экране.
   def self.find_all(limit, type)
     db = SQLite3::Database.open(SQLITE_DB_FILE)
 
@@ -132,12 +59,22 @@ class Post
     query += 'ORDER by rowid DESC '
     query += 'LIMIT :limit ' unless limit.nil?
 
-    statement = db.prepare query
+    begin
+      statement = db.prepare query
+    rescue SQLite3::SQLException => e
+      puts "Не удалось выполнить запрос в базе #{SQLITE_DB_FILE}"
+      abort e.message
+    end
 
     statement.bind_param('type', type) unless type.nil?
     statement.bind_param('limit', limit) unless limit.nil?
 
-    result = statement.execute!
+    begin
+      result = statement.execute!
+    rescue SQLite3::SQLException => e
+      puts "Не удалось выполнить запрос в базе #{SQLITE_DB_FILE}"
+      abort e.message
+    end
 
     statement.close
     db.close
@@ -145,37 +82,62 @@ class Post
     result
   end
 
-  def self.find_by_id(id)
-    return if id.nil?
+  def read_from_console
 
-    db = SQLite3::Database.open(SQLITE_DB_FILE)
-    db.results_as_hash = true
-    result = db.execute('SELECT * FROM posts WHERE  rowid = ?', id)
-    db.close
-
-    return nil if result.empty?
-
-    result = result[0]
-
-    post = create(result['type'])
-    post.load_data(result)
-    post
   end
 
-  def self.find_by_id(id)
-    return if id.nil?
+  def to_strings
 
+  end
+
+  def load_data(data_hash)
+    @created_at = Time.parse(data_hash['created_at'])
+    @text = data_hash['text']
+  end
+
+  def to_db_hash
+    {
+      'type' => self.class.name,
+      'created_at' => @created_at.to_s
+    }
+  end
+
+  def save_to_db
     db = SQLite3::Database.open(SQLITE_DB_FILE)
     db.results_as_hash = true
-    result = db.execute('SELECT * FROM posts WHERE  rowid = ?', id)
+
+    post_hash = to_db_hash
+
+    begin
+      db.execute(
+        'INSERT INTO posts (' +
+        post_hash.keys.join(', ') +
+        ") VALUES (#{('?,' * post_hash.size).chomp(',')})",
+        post_hash.values
+      )
+    rescue SQLite3::SQLException => e
+      puts "Не удалось выполнить запрос в базе #{SQLITE_DB_FILE}"
+      abort e.message
+    end
+
+    insert_row_id = db.last_insert_row_id
     db.close
+    insert_row_id
+  end
 
-    return nil if result.empty?
+  def save
+    file = File.new(file_path, 'w:UTF-8')
 
-    result = result[0]
+    to_strings.each { |string| file.puts(string) }
 
-    post = create(result['type'])
-    post.load_data(result)
-    post
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+
+    file_time = @created_at.strftime('%Y-%m-%d_%H-%M-%S')
+
+    "#{current_path}/#{self.class.name}_#{file_time}.txt"
   end
 end
