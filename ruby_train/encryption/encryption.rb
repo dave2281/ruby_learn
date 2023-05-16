b6ad7a9cb944a43de09243d0cb0acc579543fdb2
dave2281 <dava.bigboy@gmail.com>
2023-05-16 10:55:26 +0300

    encryption md5 sha1

---
+++ ruby_train/encryption/encryption.rb
@@ -0,0 +1,20 @@
+require 'digest'
+
+# text = "dave"
+
+# text1 = "dave1"
+
+# encrypt_text = Digest::SHA1.hexdigest(text)
+
+# encrypt_text1 = Digest::SHA1.hexdigest(text1)
+
+# p (encrypt_text == encrypt_text1)
+
+# p encrypt_text
+# p encrypt_text.size
+
+# text = '2004'
+
+# encrypt_text = Digest::MD5.hexdigest(text)
+
+# p encrypt_text
