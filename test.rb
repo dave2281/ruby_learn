4acb9e280bc56414c6bb755464953c5a5539fac3
dave2281 <dava.bigboy@gmail.com>
2023-08-25 15:08:14 +0300

    some changes

--- test.rb
+++ test.rb
@@ -13,20 +13,53 @@
 # p jake ||= 'Jake'
 # p mike ||= 'Mike'
 
-class BasicObject 
-  alias_method :kvas, :class
-end 
+# class BasicObject 
+#   alias_method :kvas, :class
+# end 
 
-class Piwo 
-  def intialize(name)
+# class Piwo 
+#   def intialize(name)
+#     @name = name 
+#   end
+
+#   def showName 
+#     @name 
+#   end
+# end 
+
+# napitok = Piwo.new('obolon')
+
+# p napitok.showName.kvas
+
+class Dog 
+  attr_reader :name, :klichka
+
+  def initialize(name, klichka)
     @name = name 
+    @klichka = klichka
   end
 
-  def showName 
-    @name 
+  def sound 
+    if @name == 'Charlie'
+      return 'Woof-Woof, im Charlie'
+    end
+  end 
+
+  def four_paws 
+    '4 paws'
   end
+end
+
+class Bouldog < Dog 
+  def sound 
+    super
+  end 
+  
+  def four_paws
+    'i have 2 paws'
+  end 
 end 
 
-napitok = Piwo.new('obolon')
+bouldog1 = Bouldog.new('Charlie', 'Charka')
 
-p napitok.showName.kvas
+p bouldog1.sound
